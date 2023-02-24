9a7a82e2c8f2863041c96d14b43318db542faed6
habanoz <huseyinabanox@gmail.com>
2023-02-24 10:35:55 +0300

    - q1 p1 image files.

---
+++ hw5/run-q1-p1-s2-3-cp.sh
@@ -0,0 +1,8 @@
+python -m cs285.scripts.run_hw5_expl --env_name PointmassEasy-v0 --unsupervised_exploration --exp_name q1_env1_random --seed 2 --num_exploration_steps 0
+python -m cs285.scripts.run_hw5_expl --env_name PointmassMedium-v0 --unsupervised_exploration --exp_name q1_env2_random --seed 2 --num_exploration_steps 0
+python -m cs285.scripts.run_hw5_expl --env_name PointmassHard-v0 --unsupervised_exploration --exp_name q1_env3_random --seed 2 --num_exploration_steps 0
+
+python -m cs285.scripts.run_hw5_expl --env_name PointmassEasy-v0 --unsupervised_exploration --exp_name q1_env1_random --seed 3 --num_exploration_steps 0
+python -m cs285.scripts.run_hw5_expl --env_name PointmassMedium-v0 --unsupervised_exploration --exp_name q1_env2_random --seed 3 --num_exploration_steps 0
+python -m cs285.scripts.run_hw5_expl --env_name PointmassHard-v0 --unsupervised_exploration --exp_name q1_env3_random --seed 3 --num_exploration_steps 0
+
